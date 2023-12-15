2e4b46f177ec25eaaa4b8195d2400fa5b5bff843
Mạnh Đạt Võ <dat.vomanh@haravan.com>
2023-12-15 16:45:13 +0700

    feat: create template

---
+++ api/app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require('express');
+const morgan = require("morgan");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
+const rootRouter = require("./src/routes/root.router")
+
+const app = express();
+const cors = require('cors');
+
+// * init middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+const PORT = 5001;
+
+const corsOptions = {
+    origin: `localhost:${PORT}`,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
+// * init Router
+app.use("/v1/api", rootRouter);
+
+app.get('/', (req, res) => {
+    res.send('Pong App is Working')
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on PORT ${PORT}`)
+})
---
+++ api/src/routes/root.router.js
@@ -0,0 +1,8 @@
+/**
+ * New Import for Create new Router 
+ * rootRouter.use("/shop", shopRouter);
+ */
+
+const rootRouter = require("express").Router();
+
+module.exports = rootRouter;
