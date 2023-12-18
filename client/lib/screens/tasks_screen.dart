c64f8e204d78a76ebb094097efea098d00c54e6b
Mạnh Đạt Võ <dat.vomanh@haravan.com>
2023-12-18 09:54:56 +0700

    feat: new feature learn on flutter

---
+++ client/lib/screens/tasks_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../models/tasks.dart';
+
+class TasksScreen extends StatelessWidget {
+  TasksScreen({Key? key}) : super(key: key);
+
+  List<Task> tasksList = [
+    Task(title: 'Task1'),
+    Task(title: 'Task2'),
+    Task(title: 'Task3'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tasks App"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Center(
+            child: Chip(
+              label: Text("Nhiệm vụ:"),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasksList.length,
+              itemBuilder: (context, index) {
+                var task = tasksList[index];
+
+                return ListTile(
+                  title: Text(task.title),
+                  trailing: Checkbox(
+                    value: task.isDone,
+                    onChanged: (value) {},
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: "Thêm",
+        // child: const Icon(Icon add)
+      ),
+    );
+  }
+}
