6b7b828b1796685b26c24a946cb90f8cd2da1382
Mạnh Đạt Võ <dat.vomanh@haravan.com>
2023-12-24 14:42:02 +0700

    fix: add new route

--- api/src/routes/root.router.js
+++ api/src/routes/root.router.js
@@ -1,7 +1,10 @@
 const authRouter = require("./auth.router");
+const walletRouter = require("./wallet.router");
 
 const rootRouter = require("express").Router();
 
 rootRouter.use("/auth", authRouter)
+rootRouter.use("/wallet", walletRouter)
+
 
 module.exports = rootRouter;
